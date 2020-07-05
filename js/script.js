d3d20fb3de8d517ef0a7132543c53be1636f2fb4
Alessandro <alex.ixs@hotmail.com>
2020-07-05 19:26:53 +0200

    Boolflix final version
    - Refactoring JS code.
    - Converted release-date whith moment.js.
    - Redesign error & no result messages.
    - responsive improved.
    - Eliminated pagination (for now)

--- js/script.js
+++ js/script.js
@@ -1,40 +1,28 @@
 $(document).ready(function() {
-  var queryArray = [];
-  console.log(queryArray);
 
-  // Al click sul bottone stampo i risultati della ricerca
+  // ==============================================================
+  // ====================== CLIK ON BUTTON ========================
   $('button.search-movie-btn').click(function() {
     resetSearchResult();
 
-    // Metto il valore dell'input nella variabile (function line: 36)*
-    var searchValue = queryValue();
-    console.log(searchValue);
+    var inputValue = getInputValue();
 
-    queryArray.push(searchValue);
+    ajaxCall(inputValue,'movie');
+    ajaxCall(inputValue,'tv');
 
-    console.log(queryArray);
-    // Chiamo il database di TMDb e visualizzo i risultati in base al valore della ricerca
-    ajaxCall(searchValue,'movie', 1);
-    ajaxCall(searchValue,'tv', 1);
+  }); // End click
 
-  }); // End click on search-movie-btn
-
-  // Premendo tasto enter stampo i risultati della ricerca
+  // ==============================================================
+  // ====================== KEYPRESS EVENT ========================
   $('.search-movie').keypress(function(event) {
     if (event.which === 13 ) {
       resetSearchResult();
 
-      // Metto il valore dell'input nella variabile (function line: 36)*
-      var searchValue = queryValue();
+      var inputValue = getInputValue();
 
-      queryArray.push(searchValue);
-      console.log(queryArray);
-
-      // Chiamo il database di TMDb e visualizzo i risultati in base al valore della ricerca, tipo, pagina del risultato
-      ajaxCall(searchValue,'movie', 1);
-      ajaxCall(searchValue,'tv', 1);
-
-    } // End if event.which
+      ajaxCall(inputValue,'movie');
+      ajaxCall(inputValue,'tv');
+    }
   }); // End keypress event
 
 
@@ -42,11 +30,11 @@ $(document).ready(function() {
 // ======================== FUNZIONI ============================
 // ==============================================================
 
-// ==================== searchMovie() ===========================
-// Funzione searchMovie
+// ==================== getInputValue() =========================
+// Funzione getInputValue()
 // legge il valore dell'input .search-movie
-// ---> Ritorna: valore dell'input
-function queryValue() {
+// ---> return: valore dell'input
+function getInputValue() {
     // Leggo il valore all'evento (click, keypress)
     var searchInputValue = $('input.search-movie').val();
 
@@ -55,16 +43,18 @@ function queryValue() {
 
     // Ritorno il valore dell'input
     return searchInputValue
-}; // End funzione searchMovie
+};
 // --------------------------------------------------------------
 
-// ==================== ajaxCall() ==============================
+// ===================== ajaxCall() =============================
 // Funzione ajaxCall
-// TMDb: Chiave API (v3 auth)
 // api_key: 345a41c08ec6d0c01364a6a7cd7a8052
-// Interroga il database di Themoviedatabase.org e stampo i valori
-// --->>> Argomento: variabile che è una stringa o un valore dell'input
-function ajaxCall(valoreRicerca, searchType, page) {
+// Chiama il database di Themoviedatabase.org
+// Argomenti:
+// ---> valoreRicerca = una stringa/testo
+// ---> searchType = determina endpoint dell'api può essere di 2 tipi 'tv' oppure 'movie'.
+// return: ninete
+function ajaxCall(valoreRicerca, searchType) {
   if(searchType === 'movie') {
     var url = "https://api.themoviedb.org/3/search/movie"
   } else if (searchType === 'tv') {
@@ -77,39 +67,40 @@ function ajaxCall(valoreRicerca, searchType, page) {
       data: {
         api_key:"345a41c08ec6d0c01364a6a7cd7a8052",
         query: valoreRicerca,
-        page: page,
+        page: 1,
         language: 'it-IT',
+        // include_adult: true
       },
       success: function(data) {
+        var totalResults = data.total_results
         var searchResults = data.results;
-        // console.log(searchResults);
 
         if(searchResults.length > 0) {
+
+          if(searchType === 'movie') {
+            $('.movies .results-number').text(data.total_results)
+          } else {
+            $('.tv-series .results-number').text(data.total_results)
+          }
+
+          $('.show-results p.risultati').removeClass('hidden');
 
           movieTamplate(searchResults, searchType);
 
           filmHover();
 
-          $('.select-container').removeClass('hidden');
-
-          pageSelector();
-
         } else {
-          resetSearchResult();
 
-          var noResultsMessage = "La ricerca non ha prodotto risultati";
-          printMessage(noResultsMessage);
+          noResultsMessage(searchType, data);
 
-          $('.select-container').addClass('hidden');
         }
       },
       error: function() {
         resetSearchResult();
 
-        var errore = "Qualcosa non torna";
-        printMessage(errore);
+        var errore = "<i class='fas fa-exclamation-triangle'></i>" + "<p class='first-msg'>" + "Qualcosa non torna" + "</p>" + "<p class='second-msg'>" + 'Devi inserire almeno una parola' + "</p>";
 
-        $('.select-container').addClass('hidden');
+        printMessage(errore);
       }
     }
   ); // End ajax call
@@ -117,110 +108,74 @@ function ajaxCall(valoreRicerca, searchType, page) {
 } // End function ajaxCall
 // --------------------------------------------------------------
 
-// ==================== pageSelector() ==========================
-function pageSelector() {
-  // Seleziono tag select
-  var select = $('.page-selector');
-
-  select.val(1)
-  // Cambio valore dell'opzione del tag select
-  select.change(function() {
-    // Seleziono opzione del select
-    selectOption = $(select).val();
-    var ultimaQuery = queryArray.length - 1;
-    console.log(ultimaQuery)
-    var lastWord = queryArray[ultimaQuery];
-    console.log(lastWord);
-
-    // Stampo la pagina della ricerca
-    // selezionata nel select (.page-selector)
-    $.ajax(
-      {
-        url:"https://api.themoviedb.org/3/search/multi",
-        method:"GET",
-        data: {
-          api_key:"345a41c08ec6d0c01364a6a7cd7a8052",
-          query: lastWord,
-          page: selectOption,
-          language: 'it-IT'
-        },
-        success: function(data) {
-          var searchResults = data.results;
-
-          resetSearchResult();
-
-          movieTamplate(searchResults);
-
-          filmHover();
-        },
-        error: function() {
-          alert('ERRORE')
-        }
-      }
-    ); // End ajax call
-
-  });
-}
-// --------------------------------------------------------------
-
-// ==================== movieTamplate() =========================
+// ==================== objectTamplate() =========================
 // Function movieTamplate
-// Con hendlebars compilo il tamplate
-// --->>> Argomento: un array di oggetti che ottengo con la chiamata ajax
+// Con hendlebars compilo il tamplate:
+// Argomenti:
+// ---> resultArray = un array di oggetti che ottengo con la chiamata ajax
+// ---> searchType = determina l'endpoint della chiamata API (si ottiene leggendo l'argomento della funzione "ajaxCall()"
 function movieTamplate(resultArray, searchType) {
 
   for (var i = 0; i < resultArray.length; i++) {
     // Metto nella variabile singolo oggetto dell'array
-    var sinngleMovie = resultArray[i];
-    console.log(sinngleMovie)
+    var singleObject = resultArray[i];
+    // console.log(singleObject)
 
     // Trasformazione da punteggio 10 a punteggio 5
-    var rating = sinngleMovie.vote_average
+    var rating = singleObject.vote_average
     var ratingToPrint = movieScore(rating);
 
+    // Trasformazione data di uscita nel formato leggibile
+    moment.locale('it');
+    var dataUscitaMovie = moment(singleObject.release_date).format('D MMM YYYY');
+    var dataUscitaTV = moment(singleObject.first_air_date).format('D MMM YYYY');
+
+
     // POSTER
-    if (sinngleMovie.poster_path !== null) {
-      var poster = 'https://image.tmdb.org/t/p/original' + sinngleMovie.poster_path;
+    if (singleObject.poster_path !== null) {
+      var poster = 'https://image.tmdb.org/t/p/original' + singleObject.poster_path;
     } else {
       poster = 'img/no-poster1.jpg'
     }
 
     // GENERE
-    if (searchType === 'movie' || sinngleMovie.media_type === 'movie') {
+    if (searchType === 'movie') {
       var genere = 'Film'
-      var titolo = sinngleMovie.title;
-      var titoloOriginale = sinngleMovie.original_title;
-      var uscita = sinngleMovie.release_date
+      var titolo = singleObject.title;
+      var titoloOriginale = singleObject.original_title;
+      var uscita = dataUscitaMovie;
     } else {
       // Cambio le varioabili per i risultati SerieTV
       genere = 'Serie TV';
-      titolo = sinngleMovie.name;
-      titoloOriginale = sinngleMovie.original_name;
-      uscita = sinngleMovie.first_air_date
+      titolo = singleObject.name;
+      titoloOriginale = singleObject.original_name;
+      uscita = dataUscitaTV;
     }
 
-    if (sinngleMovie.media_type != "person") {
-      // Compilo il template
-      var source = $('#movies-template').html();
-      var template = Handlebars.compile(source);
+    // Compilo il template
+    var source = $('#movies-template').html();
+    var template = Handlebars.compile(source);
 
-      // Metto nell'oggetto le chiavi del risultato e stampo i relativi valori
-      var context = {
-        // COMMON
-        "poster" : poster,
-        "genere": genere,
-        "titolo" : titolo,
-        "titolo-originale": titoloOriginale,
-        "uscita": uscita,
-        "overview": sinngleMovie.overview,
-        "lingua": sinngleMovie.original_language,
-        "voto_medio": ratingToPrint,
-      }
+    // Metto nell'oggetto le chiavi del risultato e stampo i relativi valori
+    var context = {
+      // COMMON
+      "poster" : poster,
+      "genere": genere,
+      "titolo" : titolo,
+      "titolo-originale": titoloOriginale,
+      "uscita": uscita,
+      "overview": singleObject.overview,
+      "lingua": singleObject.original_language,
+      "voto_medio": ratingToPrint,
+    }
 
-      var html = template(context);
+    var html = template(context);
 
-      // Appendo il template compilato nel container apposito
-      $('.show-results .container').append(html);
+    // Appendo il template compilato nel container apposito
+    if (searchType === 'movie') {
+      $('.show-results .container.movies-results').append(html);
+    } else {
+      $('.show-results .container.tv-series-results').append(html);
     }
   }
 }
@@ -230,7 +185,14 @@ function movieTamplate(resultArray, searchType) {
 // Funzione resetPage()
 // Azzero i risultati della pagina qualora fossero presenti
 function resetSearchResult() {
-  $('.show-results .container').html('');
+  $('.show-results p.risultati').addClass('hidden');
+
+  $('.movies .results-number').text('');
+  $('.show-results .container.movies-results').html('');
+
+  $('.tv-series .results-number').text('');
+  $('.show-results .container.tv-series-results').html('');
+
 }
 // --------------------------------------------------------------
 
@@ -249,6 +211,7 @@ function printMessage(text) {
   $('.show-results .container').append(html)
 }
 // --------------------------------------------------------------
+
 
 // ==================== movieScore() ============================
 // Funzione trasformazione voto
@@ -270,6 +233,24 @@ function movieScore(rating) {
 }
 // --------------------------------------------------------------
 
+// ==================== noResultsMessage() ============================
+// stamo il messagio quando la ricerca non ha prodotto i risultati
+function noResultsMessage(searchType, data) {
+  $('.show-results p.risultati').removeClass('hidden');
+  var noResultsMessage = '<span class="no-results-message">'+ "La ricerca non ha prodotto risultati" + '<span>';
+
+  if(searchType === 'movie') {
+    $('.movies .results-number').text(data.total_results);
+    $('.show-results .container.movies-results').html(noResultsMessage);
+  } else {
+    $('.tv-series .results-number').text(data.total_results);
+    $('.show-results .container.tv-series-results ').html(noResultsMessage);
+  }
+
+  return noResultsMessage;
+}
+// --------------------------------------------------------------
+
 // Al maousehover nascondo il poster e mostro informazioni sul film
 function filmHover() {
   $('.movie-container').each(function() {
@@ -287,27 +268,6 @@ function filmHover() {
 
   });
 }
+// --------------------------------------------------------------
 
 }) // end document ready
-
-// ==============================================================
-// =============== CODICE ELIMINATO =============================
-
-// // ==================== pageSelector() ==========================
-// function pageSelector() {
-//       var ultimaQuery = queryArray.length - 1;
-//       console.log(ultimaQuery)
-//       var lastWord = queryArray[ultimaQuery];
-//       console.log(lastWord);
-//
-//       // select.val(1)
-//       // Seleziono opzione del select
-//         var select = $('.page-selector');
-//       var selectOption = $(select).val();
-//       var pageNumber = parseInt(selectOption)
-//       console.log(pageNumber)
-//
-//       ajaxPageCall(lastWord, 'movie', pageNumber);
-//       ajaxPageCall(lastWord, 'tv', pageNumber);
-//   }
-// // --------------------------------------------------------------
